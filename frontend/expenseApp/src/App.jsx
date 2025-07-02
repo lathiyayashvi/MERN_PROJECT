00486e341c6f14c34d26b5ce01068acfc1fdee27
YashviLathiya <202201220@daiict.ac.in>
2025-07-02 11:38:12 +0530

    Fix the path issue in App.jsx file

--- frontend/expenseApp/src/App.jsx
+++ frontend/expenseApp/src/App.jsx
@@ -13,7 +13,7 @@ import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
 import Expense from "./pages/Dashboard/Expense";
-import UserProvider from "./context/userContext";
+import UserProvider from "./context/UserContext.jsx";
 import EmailVerify from "./pages/Auth/EmailVerify";
 import ResetOTP from "./pages/Auth/ResetOTP";
 import EditProfile from "./pages/Dashboard/EditProfile";
